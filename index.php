b7230a938c4071d37f5d75a96b533d95bd880943
rohangardiner <71481034+rohangardiner@users.noreply.github.com>
2020-09-18 15:18:15 +1000

    index.php
    Home page and scheduling tool

---
+++ index.php
@@ -0,0 +1,24 @@
+<div id="sInput">
+<form name="newEntry" action="">
+  <fieldset>
+    <input type="text" name="title" id="title" value="" class="text-input" placeholder="Title"/>
+     <br>
+    <input type="text" name="duration" id="duration" value="" class="text-input" placeholder="Duration"/>
+     <br>
+    <input type="text" name="startBefore" id="startBefore" value="" class="text-input" placeholder="Start before"/>
+    <br>
+    <input type="text" name="endBefore" id="endBefore" value="" class="text-input" placeholder="End before"/>
+     
+      <br />
+    <input type="submit" name="submit" class="button" id="submit_btn" value="Send" />
+  </fieldset>
+</form>
+</div>
+
+<div id="sTasks">
+[A 15 6 7 x] [B 1 5 7 x] [C 2.5 2 7 x]
+</div>
+
+<div id="sDisplay">
+Grid here
+</div>
