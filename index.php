16c4416dfcc32693fe71639706c7d85e8dadc02c
Rohan <71481034+rohangardiner@users.noreply.github.com>
2020-10-22 12:55:54 +1000

    Removed the grid! Don't need it!

--- index.php
+++ index.php
@@ -1,41 +1,17 @@
 <div id="sInput">
-<form name="newEntry" action="">
-  <fieldset>
-    <input type="text" name="title" id="title" value="" class="text-input" placeholder="Title"/>
-     <br>
-    <input type="text" name="duration" id="duration" value="" class="text-input" placeholder="Duration"/>
-     <br>
-    <input type="text" name="startBefore" id="startBefore" value="" class="text-input" placeholder="Start before"/>
-    <br>
-    <input type="text" name="endBefore" id="endBefore" value="" class="text-input" placeholder="End before"/>
-     
-      <br />
-    <input type="submit" name="submit" class="button" id="submit_btn" value="Send" />
-  </fieldset>
-</form>
+  <form name="newEntry" action="">
+    <input type="text" name="title" id="title" value="" placeholder="Title" required/><br>
+    <input type="text" name="duration" id="duration" value="" placeholder="Duration"/><br>
+    <input type="text" name="startBefore" id="startBefore" value="" placeholder="Start before"/><br>
+    <input type="text" name="endBefore" id="endBefore" value="" placeholder="End before"/><br />
+    <input type="submit" value="Create" onclick="addEntry();return false;">
+  </form>
 </div>
 
 <div id="sTasks">
-  <button class="taskItem" onclick="remove(this)">A 15 6 7 x</button>
-  <button class="taskItem" onclick="remove(this)">B 1 5 7 x</button>
-  <button class="taskItem" onclick="remove(this)">C 2.5 2 7 x</button>
-  <button class="taskItem" onclick="remove(this)">A 15 6 7 x</button>
-  <button class="taskItem" onclick="remove(this)">B 1 5 7 x</button>
-  <button class="taskItem" onclick="remove(this)">C 2.5 2 7 x</button>
-  <button class="taskItem" onclick="remove(this)">A 15 6 7 x</button>
-  <button class="taskItem" onclick="remove(this)">B 1 5 7 x</button>
-  <button class="taskItem" onclick="remove(this)">C 2.5 2 7 x</button>
-  <button class="taskItem" onclick="remove(this)">A 15 6 7 x</button>
-  <button class="taskItem" onclick="remove(this)">B 1 5 7 x</button>
-  <button class="taskItem" onclick="remove(this)">C 2.5 2 7 x</button>
+  <button class="taskItem" onclick="remove(this)">Z 8 8 8 x</button>
   <div class="taskInstructions">Click tasks to remove</div>
 </div>
 
 <div id="sDisplay">
-  <div class="box a">A</div>
-  <div class="box b">B</div>
-  <div class="box c">C</div>
-  <div class="box d">D</div>
-  <div class="box e">E</div>
-  <div class="box f">F</div>
 </div>
